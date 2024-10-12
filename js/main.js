601b5f9b8ef9f21d602b29482c281ec716acf30b
Craig Lawson <78158337+craiglawsonnn@users.noreply.github.com>
2024-10-12 15:08:42 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -23,6 +23,8 @@ let controls;
 //Set which object to render
 let objToRender = 'Craig';
 
+console.log(`models/${objToRender}/scene.gltf`);
+
 //Instantiate a loader for the .gltf file
 const loader = new GLTFLoader();
 
