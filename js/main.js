5419dd97711c6927a385cb9faffae3eb02219363
Craig Lawson <78158337+craiglawsonnn@users.noreply.github.com>
2024-10-13 20:27:32 +0100

    changing to autorotate on the model

--- js/main.js
+++ js/main.js
@@ -54,7 +54,7 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.getElementById("container3D").appendChild(renderer.domElement);
 
 //Set how far the camera will be from the 3D model
-camera.position.z = 5;  // Move the camera closer to the model
+camera.position.z = 2;  // Move the camera closer to the model
 
 //Add lights to the scene, so we can actually see the 3D model
 const topLight = new THREE.DirectionalLight(0xffffff, 1); // (color, intensity)
@@ -75,11 +75,15 @@ function animate() {
   requestAnimationFrame(animate);
   //Here we could add some code to update the scene, adding some automatic movement
 
+  // Rotate the object continuously
+  object.rotation.x += 0.01;
+  object.rotation.y += 0.01;
+
   //Make the eye move
   if (object && objToRender === "Craig") {
     //I've played with the constants here until it looked good 
-    object.rotation.y = -3 + mouseX / window.innerWidth * 3;
-    object.rotation.x = -1.2 + mouseY * 2.5 / window.innerHeight;
+    //object.rotation.y = -3 + mouseX / window.innerWidth * 3;
+    //object.rotation.x = -1.2 + mouseY * 2.5 / window.innerHeight;
   }
   renderer.render(scene, camera);
 }
